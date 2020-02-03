931ae40abc644e68d039f30425011a6fe171592c
Erwan <erwan.ari211@gmail.com>
2020-02-03 15:14:03 +0700

    Add package rajaongkir

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        erfanbagussetiana\rajaongkirpro\RajaOngkirServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'RajaOngkir' => erfanbagussetiana\rajaongkirpro\RajaOngkirFacade::class,
 
     ],
 
